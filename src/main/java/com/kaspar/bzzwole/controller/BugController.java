448470e585a2974800e117968629c2f8e7525b4c
Kaspar Minn <kaspar.minn@gmail.com>
2020-10-12 18:58:12 +0300

    Further Development

--- src/main/java/com/kaspar/bzzwole/controller/BugController.java
+++ src/main/java/com/kaspar/bzzwole/controller/BugController.java
@@ -1,8 +1,10 @@
 package com.kaspar.bzzwole.controller;
 
+import com.kaspar.bzzwole.exception.ResourceNotFoundException;
 import com.kaspar.bzzwole.model.Bug;
 import com.kaspar.bzzwole.repository.BugRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,5 +26,12 @@ public class BugController {
     // Create Bug rest API!
     @PostMapping("/bugs")
     public Bug createBug(@RequestBody Bug bug) { return bugRepository.save(bug); }
+
+    // Get BUG by rest API!
+    @GetMapping("/bugs/{id}")
+    public ResponseEntity<Bug> getBugById(@PathVariable Long id) {
+        Bug bug = bugRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Sorry, the bug: " + id + " does not exist!"));
+        return ResponseEntity.ok(bug);
+    }
 
 }
